3098e4b3a0e53dd048ba01ed366573535d5aba60
Jon McCarthy <jtmcc93@gmail.com>
2019-10-11 16:36:30 -0700

    Update index.js
    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "ae940d8b785932aa43e7d37b28ccc99e";
+const API_KEY = "";
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
